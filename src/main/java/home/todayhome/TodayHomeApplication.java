dcd494ac5387a5dd144e3e6ac3745bc489f64b80
dabin <113006964+dabinHam@users.noreply.github.com>
2023-08-08 19:17:50 +0900

    init : springMCV 적용 , config,dto,entity,exception,security 디렉토리 추가

---
+++ src/main/java/home/todayhome/TodayHomeApplication.java
@@ -0,0 +1,13 @@
+package home.todayhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/todayhome/TodayHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package home.todayhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
