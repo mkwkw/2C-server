fb599d04bed9f8be7e0b27ff745bf9c7890346e1
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-08-10 00:03:58 +0900

    fix: 작업중인 오류코드 수정
    작업중인 오류코드 수정

---
+++ src/main/java/home/todayhome/repository/BoardRepository.java
@@ -0,0 +1,25 @@
+package home.todayhome.repository;
+
+import home.todayhome.dto.BoardDto;
+import home.todayhome.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    @Query("SELECT new home.todayhome.dto.BoardDto(b.id, u.nickname, b.title, b.content, b.heartCount, b.visitorCount, b.createdAt) " +
+            "FROM Board b " +
+            "INNER JOIN User u on u = b.user "
+    )
+    List<BoardDto> findBoardPage(Pageable pageable);
+
+    @Query("SELECT b From Board b INNER JOIN User u on u = b.user where b.id = :boardId")
+    Board findByBoardId(@Param("boardId") Integer boardId);
+}
