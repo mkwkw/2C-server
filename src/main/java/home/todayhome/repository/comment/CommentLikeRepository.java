68770a42e68e4c131630d541d33c4abc08ea6f02
G1-H <hwangjiwon1992@gmail.com>
2023-08-10 00:55:30 +0900

    Refactor : 레포지토리 패키지 정리
    레포지토리 패키지 내 패키지 없앰

--- src/main/java/home/todayhome/repository/comment/CommentLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package home.todayhome.repository.comment;
-
-import home.todayhome.entity.LikesComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentLikeRepository extends JpaRepository<LikesComment, Integer> {
-}
