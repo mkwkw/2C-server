fdd77790fc677f5cf05410aea9032afdda41ae7e
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-08-10 16:43:03 +0900

    Fix :  JpaRepository 수정
    JpaRepository의
    Long -> Integer 수정

--- src/main/java/home/todayhome/repository/UserRepository.java
+++ src/main/java/home/todayhome/repository/UserRepository.java
@@ -3,7 +3,7 @@ package home.todayhome.repository;
 import home.todayhome.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Integer> {
     boolean existsByEmail(String email);
 
     User findByEmail(String email);
