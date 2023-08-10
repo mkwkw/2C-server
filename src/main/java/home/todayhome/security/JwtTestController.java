2455c04d34f7f139a75a7a608965c61caa0b3ae9
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-10 17:12:18 +0900

    Fix: swagger API 설명 추가
    swagger에 필요한 API 설명 추가하였습니다.

--- src/main/java/home/todayhome/security/JwtTestController.java
+++ src/main/java/home/todayhome/security/JwtTestController.java
@@ -1,5 +1,7 @@
 package home.todayhome.security;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +19,20 @@ public class JwtTestController {
 
     private final JwtService jwtService;
 
+    @ApiOperation("사용자 email과 id를 이용해서 jwt token 생성")
     @GetMapping("/encode")
-    public String jwtEncode(@RequestParam("email") String userEmail, @RequestParam("user-id") Integer userId){
+    public String jwtEncode(@ApiParam(name = "email", value = "user email", example = "abc@de.com") @RequestParam("email") String userEmail, @ApiParam(name = "user-id", value = "user id", example = "1") @RequestParam("user-id") Integer userId){
         return jwtService.encode(userEmail, userId);
     }
 
+    @ApiOperation("Jwt token 복호화해서 user email과 id 얻기")
     @GetMapping("/decode")
-    public Map<String, Object> jwtDecode(@RequestParam("token") String jwtToken){
+    public Map<String, Object> jwtDecode(@ApiParam(name = "token", value = "jwt token(by email and id)", example = "euKdfkj~~") @RequestParam("token") String jwtToken){
         return jwtService.decode(jwtToken);
     }
 
+    //TODO. swagger에서 테스트해보려면 따로 헤더 설정 필요
+    @ApiOperation("Request Header에서 얻은 Jwt token으로 user email과 id 얻기")
     @GetMapping
     public void getMemberGeneratedId(@TokenEmailAndId AuthInfo authInfo){
         log.info("authinfo={}", authInfo);
