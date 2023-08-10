bf698f05904874494d27fdcdecd0442e39a8c2c0
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-08-10 16:12:39 +0900

    Feat : 회원가입, 로그인 ,로그아웃 기능 추가
    -회원가입
    이메일과 비밀번호를 입력 받아 DB에 회원 정보를 저장합니다.
    
    - 로그인
    이메일과 비밀번호를 입력 받아 DB에 저장되어 있는 이메일과 비교 후 맞다면 로그인을 진행합니다.
    
    - 로그아웃
    이메일을 입력 후 로그아웃을 합니다.
    
    - security는 사용하지 않았습니다.

---
+++ src/main/java/home/todayhome/dto/user/LoginRequest.java
@@ -0,0 +1,14 @@
+package home.todayhome.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+
+}
