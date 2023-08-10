06aa60fe469b1af01cce40ded30d5e3619181a08
G1-H <hwangjiwon1992@gmail.com>
2023-08-11 00:04:17 +0900

    Refacotr : Swagger 문서화 작업 진행
    Swagger 문서화 작업 진행

--- src/main/java/home/todayhome/dto/LikesResponse.java
+++ src/main/java/home/todayhome/dto/LikesResponse.java
@@ -1,5 +1,6 @@
 package home.todayhome.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,11 @@ import lombok.Setter;
 @Setter
 @Builder
 public class LikesResponse {
+
+    @ApiModelProperty(name="likeId", value = "likes ID", example = "1")
     private Integer likesId;
+    @ApiModelProperty(name="isLiked", value = "좋아요 여부", example = "true")
     private Boolean isLiked;
+    @ApiModelProperty(name="likesAtContent", value = "좋아요가 적용된 댓글 혹은 게시글의 총 좋아요 수", example = "2")
     private Integer likesAtContent;
 }
