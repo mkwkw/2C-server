f13b23bbaa084ef1113ea072b860759159b7659c
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-08-11 10:17:23 +0900

    Refactor : swagger 문서화 작업 진행
    제곧내

--- src/main/java/home/todayhome/controller/UserController.java
+++ src/main/java/home/todayhome/controller/UserController.java
@@ -6,6 +6,7 @@ import home.todayhome.dto.user.SignupRequest;
 import home.todayhome.dto.user.SignupResponse;
 import home.todayhome.service.UserService;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,16 +22,19 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-
+    @ApiOperation("회원가입")
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody SignupRequest signupRequest) {
         userService.signup(signupRequest);
         return ResponseEntity.ok(new SignupResponse("회원가입이 완료되었습니다."));
     }
+
+    @ApiOperation("로그인")
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         return userService.login(loginRequest);
     }
+    @ApiOperation("로그아웃")
     @PostMapping("/logout")
     public ResponseEntity<Map<String,String>> logout(@RequestBody LogoutRequest logoutRequest) {
         return userService.logout(logoutRequest);
