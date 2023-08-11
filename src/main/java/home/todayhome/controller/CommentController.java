1d7d39e726fd0993ac3a6cf4e43be1cf58c18a9b
dabin <113006964+dabinHam@users.noreply.github.com>
2023-08-11 12:56:37 +0900

    Refactor : isDeleted 기본 값 수정 및 , 불필요한 request 삭제
    - isDeleted default is NULL -> default = false로 수정.
    - save()대신 JPQL을 사용하여 코드길이 단축
    
    - 사용하지않는 request 삭제

--- src/main/java/home/todayhome/controller/CommentController.java
+++ src/main/java/home/todayhome/controller/CommentController.java
@@ -4,6 +4,8 @@ import home.todayhome.dto.CommentDto;
 import home.todayhome.security.AuthInfo;
 import home.todayhome.security.TokenEmailAndId;
 import home.todayhome.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,12 +16,18 @@ import java.util.List;
 
 
 @RestController
-@RequiredArgsConstructor()
+@RequiredArgsConstructor
+@Api(tags = "댓글 API")
 @RequestMapping("/comments")
 public class CommentController {
 
+
     private final CommentService commentService;
 
+    @ApiOperation(
+            value = "댓글 작성",
+            notes = "회원만 댓글 작성 가능"
+    )
     @PostMapping ("/")
     public ResponseEntity<?> createComment(
             @TokenEmailAndId AuthInfo authInfo,
@@ -28,6 +36,10 @@ public class CommentController {
         return ResponseEntity.ok(commentService.createComment(createCommentRequest, authInfo.getUserEmail()));
     }
 
+    @ApiOperation(
+            value = "댓글 조회",
+            notes = "특정 게시글의 해당되는 댓글을 조회, 조회는 비회원도 가능"
+    )
     @GetMapping("/")
     public List<CommentDto.CommentResponse> getComments(
             @Valid @RequestParam Integer boardId
@@ -35,6 +47,11 @@ public class CommentController {
         return commentService.getComments(boardId);
     }
 
+    @ApiOperation(
+            value = "댓글 수정",
+            notes = "댓글 수정되면 수정시간이 업데이트" +
+                    "본인이 쓴 댓글만 수정가능"
+    )
     @PutMapping("/{commentId}")
     public CommentDto.CommentResponse updateComment(
             @TokenEmailAndId AuthInfo authInfo,
@@ -45,14 +62,18 @@ public class CommentController {
         return commentService.updateComment(authInfo.getUserEmail(), commentId, patchCommentRequest);
     }
 
+    @ApiOperation(
+            value = "댓글 삭제",
+            notes = "댓글이 삭제되면 isDeleted(soft delete) = true 로 변경" +
+                    "삭제된 게시글은 조회되지 않음"
+    )
     @PatchMapping("/{commentId}")
     public ResponseEntity<String> deleteComment(
             @TokenEmailAndId AuthInfo authInfo,
-            @PathVariable Integer commentId,
-            @RequestBody @Validated CommentDto.DeleteCommentRequest deleteCommentRequest
+            @PathVariable Integer commentId
     ){
 
-        commentService.deleteComment(authInfo.getUserEmail(), commentId, deleteCommentRequest);
+        commentService.deleteComment(authInfo.getUserEmail(), commentId);
         return ResponseEntity.ok("삭제 완료");
     }
 }
--- src/main/java/home/todayhome/repository/CommentRepository.java
+++ src/main/java/home/todayhome/repository/CommentRepository.java
@@ -3,6 +3,9 @@ package home.todayhome.repository;
 
 import home.todayhome.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +15,9 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     List<Comment> findAllByBoardId(Integer boardId);
 
-    List<Comment> findAllByBoardIdAndIsDeletedOrIsDeletedIsNull(Integer boardId, boolean isDeleted);
+    List<Comment> findAllByBoardIdAndIsDeletedFalse(Integer boardId);
+
+    @Query(value = "update Comment as c set c.isDeleted = true where c.id = :commentId")
+    @Modifying
+    void deleteByComment(@Param("commentId") Integer commentId);
 }
